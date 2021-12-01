5a2fb18d5988fc3a312b1b881edafcc08fbd10fb
Henrik Ilgen <henrik.ilgen@aclue.de>
2021-12-01 18:37:04 +0100

    Solution for day 1, puzzle 2

--- day-1/init.lua
+++ day-1/init.lua
@@ -12,10 +12,35 @@ local function puzzle1()
             function(curr, _, next) 
                 return { curr = curr, next = next } 
             end)
-        :where(function(t, k) return t.next > t.curr end)
+        :where(function(t) return t.next > t.curr end)
+        :count()
+end
+
+local function puzzle2()
+    -- This is an extremely messy way of building up the sliding window,
+    -- it'd be way cooler to have this built-in in lazylualinq!
+    local windows = measurements:zip(
+        measurements:skip(1):zip(
+            measurements:skip(2),
+            function(b, _, c) return { b = b, c = c} end
+        ),
+        function(a, _, tuple)
+            return { a = a, b = tuple.b, c = tuple.c }
+        end
+    )
+
+    local sums = windows:select(function(w) return w.a + w.b + w.c end)
+
+    return sums:zip(
+            sums:skip(1),
+            function(curr, _, next)
+                return { curr = curr, next = next }
+            end)
+        :where(function(t) return t.next > t.curr end)
         :count()
 end
 
 return {
     puzzle1 = puzzle1(),
+    puzzle2 = puzzle2(),
 }
--- main.lua
+++ main.lua
@@ -2,3 +2,4 @@ local client = require("client")
 local day1 = require("day-1")
 
 print("Answer for day 1, puzzle 1: " .. day1.puzzle1)
+print("Answer for day 1, puzzle 1: " .. day1.puzzle2)
